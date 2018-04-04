aae645b2f904f8cafd7343749bf296425cc6de7f
Lau Llobet <lau.llobet@gmail.com>
2018-04-04 16:48:26 +0200

    world can store many cells

--- src/main/java/gameoflife/World.java
+++ src/main/java/gameoflife/World.java
@@ -1,12 +1,26 @@
 package gameoflife;
 
+import java.util.HashMap;
+
 public class World {
-    Cell cell = new Cell(false);
-    public Cell getCell(int x, int y) {
-        return cell;
+    HashMap<Coordinate,Cell> hashMap = new HashMap<Coordinate, Cell>();
+
+    private Cell cell = new Cell(false);
+
+    public Cell getCell(Coordinate coordinate) {
+        if(hashMap.containsKey(coordinate)) {
+            return hashMap.get(coordinate);
+        }
+        putCell(coordinate, new Cell(false));
+        return getCell(coordinate);
     }
 
-    public void putCell(int x, int y, Cell cell) {
-        this.cell = cell;
+    public void putCell(Coordinate coordinate, Cell cell) {
+        hashMap.put(coordinate,cell);
+    }
+
+    public int countNeighbours(Coordinate coordinate) {
+        int neighbours = 6;
+        return neighbours;
     }
 }
--- src/test/java/gameoflife/WorldShould.java
+++ src/test/java/gameoflife/WorldShould.java
@@ -1,6 +1,7 @@
 package gameoflife;
 
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
@@ -8,23 +9,47 @@ import static org.junit.Assert.assertThat;
 
 public class WorldShould {
     @Test
-    public void start_with_all_cells_dead(){
+    public void start_with_all_cells_dead() {
         World world = new World();
 
-        Cell cell = world.getCell(1,1);
+        Cell cell = world.getCell(new Coordinate(1, 1));
 
-        assertThat(cell.isAlive(),is(false));
+        assertThat(cell.isAlive(), is(false));
     }
 
     @Test
-    public void store_cells_at_postion(){
+    public void store_cells_at_postion() {
         World world = new World();
 
         Cell cell = new Cell(false);
-        world.putCell(1,1,cell);
-        Cell recoveredCell = world.getCell(1,1);
+        world.putCell(new Coordinate(1, 1), cell);
+        Cell recoveredCell = world.getCell(new Coordinate(1, 1));
 
 
-        assertEquals(cell,recoveredCell);
+        assertEquals(cell, recoveredCell);
     }
+
+    @Test
+    public void count_neigbours_at_position_in_board(){
+        World world = new World();
+        Cell alive = new Cell(true);
+
+
+        world.putCell(new Coordinate(0,0), alive);
+        world.putCell(new Coordinate(1,0), alive);
+        world.putCell(new Coordinate(2,0), alive);
+
+        world.putCell(new Coordinate(0,1), alive);
+        world.putCell(new Coordinate(2,1), alive);
+
+        world.putCell(new Coordinate(0,2), alive);
+        world.putCell(new Coordinate(1,2), alive);
+        world.putCell(new Coordinate(2,2), alive);
+
+        int neighbours = world.countNeighbours(new Coordinate(1,1));
+
+
+        assertThat(neighbours,is(6));
+    }
+
 }
