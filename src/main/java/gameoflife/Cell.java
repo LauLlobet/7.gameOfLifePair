b6103e0a94f36c92a43a253a3aa2726b181a4c63
Lau Llobet <lau.llobet@gmail.com>
2018-04-04 12:32:13 +0200

    cell normal population passes without parametrization

--- src/main/java/gameoflife/Cell.java
+++ src/main/java/gameoflife/Cell.java
@@ -1,15 +1,21 @@
 package gameoflife;
 
 public class Cell {
+    private boolean alive = true;
+
     public void setAlive() {
 
     }
 
     public void nextState(int neigbours) {
-
+        if(neigbours == 2){
+            alive = true;
+            return;
+        }
+        alive = false;
     }
 
     public boolean isAlive() {
-        return false;
+        return alive;
     }
 }
--- src/test/java/gameoflife/CellShould.java
+++ src/test/java/gameoflife/CellShould.java
@@ -1,16 +1,33 @@
 package gameoflife;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class CellShould {
     @Test
-    public void die_of_underpopulation(){
+    @Parameters({ "0",
+                    "1"})
+    public void die_of_underpopulation(int neighbours){
         Cell cell = new Cell();
-        cell.setAlive();
-        cell.nextState(1);
-        assertThat(cell.isAlive(),is(false));
+
+        cell.nextState(neighbours);
+
+        assertThat(cell.isAlive(), is(false));
     }
+
+    @Test
+    public void keep_alive_if_has_normal_population(){
+        Cell cell = new Cell();
+
+        cell.nextState(2);
+
+        assertThat(cell.isAlive(),is(true));
+    }
+
 }
